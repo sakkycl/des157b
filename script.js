51408685307ccefb32d34c2c97b639e70818bb7b
Sally Lee <slllee@ucdavis.edu>
2024-04-09 04:09:32 -0700

    updated portal page

---
+++ script.js
@@ -0,0 +1,31 @@
+(function() {
+    'use strict';
+
+    const button = document.querySelector('button');
+    const body = document.querySelector('body');
+    const banner = document.querySelector('#banner');
+    const sections = document.querySelectorAll('section')
+    let mode = 'under';
+
+    button.addEventListener('click', function() {
+        if (mode === 'under') {
+            body.className = 'switch';
+            banner.className = 'switch';
+            button.className = 'switch';
+            for (const section of sections) {
+                section.className = 'switch';
+            }
+            button.innerText = "surface";
+            mode = 'sea';
+        } else {
+            body.removeAttribute('class');
+            banner.removeAttribute('class');
+            button.removeAttribute('class');
+            for (const section of sections) {
+                section.removeAttribute('class');
+            }
+            button.innerText = "dive in";
+            mode = 'under'
+        }
+    })
+})()
