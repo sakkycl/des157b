7df77bd43b0b421ac824fef72099c748476973e3
Sally Lee <slllee@ucdavis.edu>
2024-05-30 05:48:25 -0700

    updated version1

--- capstone/development/version1/script.js
+++ capstone/development/version1/script.js
@@ -8,6 +8,12 @@
     const gamecontrol = document.querySelector('#gamecontrol');
     const game = document.querySelector('#game');
 
+    //usability testing
+    const taskpopup = document.querySelector('#taskpopup');
+    taskpopup.addEventListener('click', function(){
+        taskpopup.className = 'hidden';
+    })
+
     //pancake option
     pancakebtn.addEventListener('click', function(){
 
@@ -148,10 +154,118 @@
         }
         //check opened popups
         function checkAllOpened() {
+
             if (popups.every(popup => popup.opened)) {
+
+                //cooking button
                 gamecontrol.innerHTML = '<button id="cookbtn">COOK</button> <button id="quitbtn">QUIT GAME</button>';
                 document.getElementById('cookbtn').addEventListener("click", function(){
-                    game.innerHTML = '<p>(pretend cooking animation is here)</p>';
+
+                    //cooking animation
+                    game.innerHTML = '<div id="cookpanimation"> <div id="cookp1"><img src="images/cookp1.png" alt="cookp1"></div> <div id="cookp2"><img src="images/cookp2.png" alt="cookp2"></div> <div id="cookp3"><img src="images/cookp3.png" alt="cookp3"></div> </div>';
+                    gamecontrol.style.paddingTop = "35%";
+
+                    //updated header and gamecontrol
+                    header.innerHTML = '<h1>COOKING...</h1>';
+                    gamecontrol.innerHTML = '<button id="obtainbtn">OBTAIN</button> <button id="quitbtn">QUIT GAME</button>';
+                    document.getElementById('obtainbtn').addEventListener("click", function(){
+
+                        //final touches content
+                        game.innerHTML = '<div id="final"> <div id="fcake"> <div id="pcake"><img src="images/fcake.png" alt="fcake"></div><div id="scake" class="hidden"><img src="images/scake.png" alt="scake"></div><div id="bcake" class="hidden"><img src="images/bcake.png" alt="bcake"></div> </div> <div id="maplesyrup"><img src="images/maplesyrup.png" alt="maplesyrup"><p>MAPLE SYRUP</p></div> <div id="berries"><img src="images/berries.png" alt="berries"><p>BERRIES</p></div> </div>';
+
+                        //final touches header and gamecontrol
+                        header.innerHTML = '<h1>ADD FINAL TOUCHES</h1>';
+                        gamecontrol.innerHTML = '<button id="quitbtn">QUIT GAME</button>';
+                        document.getElementById('quitbtn').addEventListener("click", function(){
+                            location.reload();
+                        });
+
+                        //final touches popups
+                        //input values
+                        const maplesyrup = document.querySelector('#maplesyrup');
+                        const berries = document.querySelector('#berries');
+                        //popups
+                        const fpopups = [
+                            {
+                            fpopup: document.getElementById('maplesyruppopup'),
+                            fpopuptext: document.getElementById('maplesyruptext'),
+                            ftextSet: [
+                                ['First message 1A: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Second message 1A: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Third message 1A: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Fourth message 1A: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Fifth message 1A: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.']
+                            ],
+                            fcurrentSetIndex: 0,
+                            fchangeCount: 0,
+                            fopened: false,
+                            div: document.getElementById('scake'),
+                            },
+                            {
+                            fpopup: document.getElementById('berriespopup'),
+                            fpopuptext: document.getElementById('berriestext'),
+                            ftextSet: [
+                                ['First message 2A: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Second message 2A: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Third message 2A: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Fourth message 2A: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Fifth message 2A: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.']
+                            ],
+                            fcurrentSetIndex: 0,
+                            fchangeCount: 0,
+                            fopened: false,
+                            div: document.getElementById('bcake'),
+                            }
+                        ];
+                        //popups text
+                        function fchangeText(index) {
+                            const fpopupObj = fpopups[index];
+                            if (fpopupObj.fchangeCount < 5) {
+                            fpopupObj.fpopuptext.textContent = fpopupObj.ftextSet[fpopupObj.fcurrentSetIndex][fpopupObj.fchangeCount];
+                            fpopupObj.fchangeCount++;
+                            } else {
+                            fpopupObj.fpopup.classList.add('hidden');
+                            document.removeEventListener('keydown', fpopupObj.keydownHandler);
+                            fpopupObj.fpopup.removeEventListener('click', fpopupObj.clickHandler);
+                            }
+                            fpopupObj.fopened = true;
+                            fcheckAllOpened();
+                            fpopupObj.div.classList.replace('hidden', 'showing');
+                        }
+                        //open popups
+                        function fopenpopup(index) {
+                            const fpopupObj = fpopups[index];
+                            //popupObj.popuptext.textContent = '';
+                            fpopupObj.fchangeCount = 0;
+                            fpopupObj.fcurrentSetIndex = (fpopupObj.fcurrentSetIndex + 1) % fpopupObj.ftextSet.length;
+                            fpopupObj.fpopup.classList.remove('hidden');
+
+                            fpopupObj.keydownHandler = () => fchangeText(index);
+                            fpopupObj.clickHandler = () => fchangeText(index);
+
+                            document.addEventListener('keydown', fpopupObj.keydownHandler);
+                            fpopupObj.fpopup.addEventListener('click', fpopupObj.clickHandler);
+                        }
+                        //check opened popups
+                        function fcheckAllOpened() {
+                            if (fpopups.every(fpopup => fpopup.fopened)) {
+
+                                //plate button
+                                gamecontrol.innerHTML = '<button id="platebtn">PLATE</button> <button id="quitbtn">QUIT GAME</button>';
+                                document.getElementById('quitbtn').addEventListener("click", function(){
+                                    location.reload();
+                                });
+                                document.getElementById('platebtn').addEventListener("click", function(){
+                                    header.innerHTML = '<h1>THANK YOU FOR PLAYING</h1>'
+                                    game.innerHTML = '';
+                                    gamecontrol.innerHTML = '<div id="thankyou"> <img src="images/finpancake.png" alt="finpancake"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><button id="playbtn">PLAY AGAIN</button> </div>';
+                                    document.getElementById('playbtn').addEventListener("click", function(){
+                                        location.reload();
+                                    });
+                                });
+                            }
+
+                        }
+                        maplesyrup.addEventListener('click', () => fopenpopup(0));
+                        berries.addEventListener('click', () => fopenpopup(1));
+
+                    });
+                    document.getElementById('quitbtn').addEventListener("click", function(){
+                        location.reload();
+                    });
+
                 });
                 document.getElementById('quitbtn').addEventListener("click", function(){
                     location.reload();
@@ -186,6 +300,7 @@
         });
         //gather ingredients content
         game.innerHTML = '<div id="ingredients"><div id="flour"><img src="images/flour.png" alt="flour"><p>FLOUR</p></div><div id="egg"><img src="images/egg.png" alt="egg"><p>EGG</p> </div><div id="milk"><img src="images/milk.png" alt="milk"><p>MILK</p></div><div id="butter"><img src="images/butter.png" alt="butter"><p>BUTTER</p></div> </div> <div id="ingredients2"> <div id="bakingpowder"><img src="images/bakingpowder.png" alt="bakingpowder"><p>BAKING POWDER</p></div> <div id="sugar"><img src="images/sugar.png" alt="sugar"><p>SUGAR</p></div> <div id="salt"><img src="images/salt.png" alt="salt"><p>SALT</p></div> <div id="vanillaextract"><img src="images/vanillaextract.png" alt="vanillaextract"><p>VANILLA EXTRACT</p></div></div>';
+        document.querySelector('#game img').style.maxWidth = '25%';
 
         //ingredients popups
 
@@ -333,16 +448,6 @@
         sugar.addEventListener('click', () => openpopup(5));
         salt.addEventListener('click', () => openpopup(6));
         vanillaextract.addEventListener('click', () => openpopup(7));
-
-        /* //header
-        header.innerHTML = '<h1>GATHER INGREDIENTS</h1>';
-        //quit button
-        gamecontrol.innerHTML = '<button id="quitbtn">QUIT GAME</button>';
-        document.getElementById('quitbtn').addEventListener("click", function(){
-            location.reload();
-        });
-        //game starts
-        game.innerHTML = '<div id="ingredients"> <div id="flour"><img src="images/flour.png" alt="flour"><p>FLOUR</p></div> <div id="egg"><img src="images/egg.png" alt="egg"><p>EGG</p> </div><div id="milk"><img src="images/milk.png" alt="milk"><p>MILK</p></div> <div id="butter"><img src="images/butter.png" alt="butter"><p>BUTTER</p></div></div>'; */
         
     })
 
